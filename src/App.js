26e986ae1340f458513834b4dc457d34a17f7d2e
Parnian Naderi <negarintarlan@gmail.com>
2022-01-03 21:39:19 +0330

    Added developer info and styled it

--- src/App.js
+++ src/App.js
@@ -3,8 +3,29 @@ import WeatherAppForm from "./WeatherAppForm";
 
 function App() {
   return (
-    <div className="App">
+    <div className="App container-fluid">
       <WeatherAppForm />
+      <div className="row mt-4">
+        <div className="dev-details">
+          <a
+            href="https://github.com/parnian-naderi/react-weather-app"
+            target="_blank"
+            rel="noreferrer noopener"
+            className="source-code"
+          >
+            Open source code
+          </a>{" "}
+          developed by{" "}
+          <a
+            href="https://parnian.netlify.app"
+            target="_blank"
+            rel="noreferrer noopener"
+            className="dev-github"
+          >
+            Parnian Naderi
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
